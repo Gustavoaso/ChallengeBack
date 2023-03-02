e94303707a1419784f7d4f0b3ea1e1d5ccef74be
Gustavo <gtvantonio@gmail.com>
2023-03-02 02:46:18 -0300

    teste commit

---
+++ SistemaDeCadastro/Controllers/empregadocontroller.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeCadastro.Interfaces;
+using SistemaDeCadastro.Models;
+
+namespace SistemaDeCadastro.Controllers
+
+    {
+    [Route("api/[controller/")]
+    [ApiController]
+    public class empregadocontroller : Controller
+    {
+
+
+        private readonly IEmpregadoRepositorio _empregadoRepositorio;
+
+        public empregadocontroller (IEmpregadoRepositorio empregadoRepositorio)
+        {
+            _empregadoRepositorio = empregadoRepositorio;
+        }
+
+        [HttpGet]
+        public async  Task<ActionResult<List<EmpregadoModel>>> BuscarTodosEmpregados()
+        
+        {
+            List<EmpregadoModel> empregado = await _empregadoRepositorio.BuscarTodosEmpregados();
+            return Ok(empregado);
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<EmpregadoModel>> BuscarPorId(int id)
+
+        {
+            EmpregadoModel  empregado = await _empregadoRepositorio.BuscarPorId(id);
+            return Ok(empregado);
+
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<EmpregadoModel>> Cadastrar([FromBody] EmpregadoModel empregadoModel)
+        {
+           EmpregadoModel empregado = await _empregadoRepositorio.Adicionar(empregadoModel);
+
+            return Ok(empregado);
+        }
+
+
+
+
+
+
+    }
+       
+    
+}
--- SistemaDeCadastro/Models/EmpregadoModel.cs
+++ SistemaDeCadastro/Models/EmpregadoModel.cs
@@ -4,15 +4,10 @@
     {
         public int IdEmpregado { get; set; }
         public string PrimeiroNome { get; set; }
-        public int UltimoNome { get; set; }
+        public string UltimoNome { get; set; }
         public int  Telefone { get; set; }
 
         public string Endereco { get; set; }
-
-
-
-
-
 
 
     }
